cad4a6d9ce598e046b0b35dec7276fed405a22c6
Ymri <1448265203@qq.com>
2023-12-17 16:44:17 +0800

    Initial commit

---
+++ docs/.vitepress/blog-theme.ts
@@ -0,0 +1,63 @@
+// 主题独有配置
+import { getThemeConfig } from '@sugarat/theme/node'
+
+// 开启RSS支持（RSS配置）
+// import type { Theme } from '@sugarat/theme'
+
+// const baseUrl = 'https://sugarat.top'
+// const RSS: Theme.RSSOptions = {
+//   title: '粥里有勺糖',
+//   baseUrl,
+//   copyright: 'Copyright (c) 2018-present, 粥里有勺糖',
+//   description: '你的指尖,拥有改变世界的力量（大前端相关技术分享）',
+//   language: 'zh-cn',
+//   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+//   favicon: 'https://sugarat.top/favicon.ico',
+// }
+
+// 所有配置项，详见文档: https://theme.sugarat.top/
+const blogTheme = getThemeConfig({
+  // 开启RSS支持
+  // RSS,
+
+  // 搜索
+  // 默认开启pagefind离线的全文搜索支持（如使用其它的可以设置为false）
+  // 如果npx pagefind 时间过长，可以手动将其安装为项目依赖 pnpm add pagefind
+  // search: false,
+
+  // 页脚
+  footer: {
+    // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
+    copyright: 'MIT License | Ymri',
+    icpRecord: {
+      name: '湘ICP备19020956号',
+      link: 'https://beian.miit.gov.cn/'
+    },
+  },
+
+  // 主题色修改
+  themeColor: 'el-blue',
+
+  // 文章默认作者
+  author: 'Ymri',
+
+  // 友链
+  friend: [
+    {
+      nickname: '粥里有勺糖',
+      des: '你的指尖用于改变世界的力量',
+      avatar:
+        'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+      url: 'https://sugarat.top',
+    },
+    {
+      nickname: 'Vitepress',
+      des: 'Vite & Vue Powered Static Site Generator',
+      avatar:
+        'https://vitepress.dev/vitepress-logo-large.webp',
+      url: 'https://vitepress.dev/',
+    },
+  ],
+})
+
+export { blogTheme }
---
+++ docs/.vitepress/config.mts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress'
+
+// 导入主题的配置
+import { blogTheme } from './blog-theme'
+
+// Vitepress 默认配置
+// 详见文档：https://vitepress.dev/reference/site-config
+export default defineConfig({
+  // 继承博客主题(@sugarat/theme)
+  extends: blogTheme,
+  base: '/blog/',
+  ignoreDeadLinks: true,
+  lang: 'zh-cn',
+  title: "Ymri",
+  description: '逃离现实|思考',
+  lastUpdated: true,
+  head: [['link', { rel: 'icon', href: './favicon.ico' }]],
+  themeConfig: {
+    lastUpdatedText: '上次更新于',
+    logo: '/logo.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '关于作者', link: 'https://github.com/ymriri' }
+    ],
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/ymriri'
+      }
+    ],
+    search: {
+      provider: 'local'
+    }
+  }
+})
